c4d1b28dd65a9cd9943f3db6e17b7d04040cf48e
kunjpatel6151 <kunjkumar6151@gmail.com>
2024-08-11 10:24:07 +0000

    Code challenge submission for question: 1342 Number of Steps to Reduce a Number to Zero

---
+++ 1342 Number of Steps to Reduce a Number to Zero/1342number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int s=0;
+        while(num!=0){
+            if(num%2==0){
+                num=num/2;
+            }
+            else{
+                num--;
+            }
+            s++;
+        }
+        return s;
+    }
+};

b6c1f5f03af9a8703cf7901dcd4f2fbd069097d5
kunjpatel6151 <kunjkumar6151@gmail.com>
2024-10-06 16:22:38 +0000

    Code challenge submission for question: 215 Kth Largest Element in an Array

---
+++ 215 Kth Largest Element in an Array/215kth-largest-element-in-an-array.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int findKthLargest(std::vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        int n=nums.size();
+        for (int i=0;i<n;i++) {
+            minHeap.push(nums[i]);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        
+        return minHeap.top();
+    }
+};

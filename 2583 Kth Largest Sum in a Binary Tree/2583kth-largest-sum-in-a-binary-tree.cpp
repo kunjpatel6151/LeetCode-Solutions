82d71b51ea6c877f7408725a8d8e40fc64212ae9
kunjpatel6151 <kunjkumar6151@gmail.com>
2024-11-15 09:51:09 +0000

    Code challenge submission for question: 2583 Kth Largest Sum in a Binary Tree

---
+++ 2583 Kth Largest Sum in a Binary Tree/2583kth-largest-sum-in-a-binary-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    #define ll long long
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        priority_queue<ll,vector<ll>,greater<ll>> minheap;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            ll levelSum=0;
+            int size=q.size();
+            for(int i=0;i<size;++i){
+                TreeNode* curr=q.front();
+                q.pop();
+
+                levelSum+=curr->val;
+                if(curr->left)  q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            minheap.push(levelSum);
+            if(minheap.size()>k)
+                minheap.pop();
+        }
+        if(minheap.size()<k)
+            return -1;
+        return minheap.top();
+    }
+};

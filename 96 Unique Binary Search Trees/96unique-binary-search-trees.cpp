3f5354930ba899e68b8f36b9b190c673b6acd5f7
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-06-14 08:48:13 +0000

    Code challenge submission for question: 96 Unique Binary Search Trees

---
+++ 96 Unique Binary Search Trees/96unique-binary-search-trees.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        int a[n+1];
+        a[0]=1;
+        a[1]=1;
+        for(int i=2;i<=n;i++){
+            a[i]=0;
+            for(int j=1;j<=i;j++){
+                a[i]+=a[j-1]*a[i-j];
+            }
+        }
+        return a[n];
+    }
+};

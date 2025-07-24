28c2132d293b75f49e58b17b8b340b5efaad6ae4
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-07-24 10:42:58 +0000

    Code challenge submission for question: 506 Relative Ranks

---
+++ 506 Relative Ranks/506relative-ranks.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n=score.size();
+        priority_queue<pair<int,int>> pq;
+        vector<string> ans(n);
+        for(int i=0;i<n;i++){
+            pq.push({score[i],i});
+        }
+        for(int i=0;i<n;i++){
+            if(i==0){
+                ans[pq.top().second]="Gold Medal";
+            }
+            else if(i==1){
+                ans[pq.top().second]="Silver Medal";
+            }
+            else if(i==2){
+                ans[pq.top().second]="Bronze Medal";
+            }
+            else{
+                 ans[pq.top().second]=to_string(i+1);
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};

148ae2df728a1fb14f71b7f3a2903cee526fd079
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-08-05 13:24:32 +0000

    Code challenge submission for question: 1539 Kth Missing Positive Number

---
+++ 1539 Kth Missing Positive Number/1539kth-missing-positive-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(arr[m]-m-1<k){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return l+k;
+    }
+};

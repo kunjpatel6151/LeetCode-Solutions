d6eba03038c4d818938b94259d595f07b0fe0f67
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-08-09 05:15:49 +0000

    Code challenge submission for question: 231 Power of Two

---
+++ 231 Power of Two/231power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n>0){
+            if((n & (n-1))==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+};

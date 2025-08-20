f9baacd8e13d6fe67798ac525a9d2e354af82f77
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-08-20 13:45:16 +0000

    Code challenge submission for question: 142 Linked List Cycle II

---
+++ 142 Linked List Cycle II/142linked-list-cycle-ii.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        bool flag=false;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                flag=true;
+                break;
+            }
+        }
+        if(flag){
+            ListNode* temp=head;
+            while(temp!=slow){
+                temp=temp->next;
+                slow=slow->next;
+            }
+            return temp;
+        }
+        else{
+            return NULL;
+        }
+    }
+};

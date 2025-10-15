c409580fe587a5c62b12e308494845eb93c83b13
kunjpatel6151 <kunjkumar6151@gmail.com>
2025-10-15 16:36:14 +0000

    Code challenge submission for question: 518 Coin Change II

---
+++ 518 Coin Change II/518coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(amount+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        } 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j>=coins[i-1]){
+                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j]; //include
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]; //exclude
+                }
+            }
+        }
+        return dp[n][amount];
+
+
+    }
+};

70e26d1ef57c5ace9d7f273be0334acaa58cc2ab
kunjpatel6151 <kunjkumar6151@gmail.com>
2026-01-20 03:44:00 +0000

    Code challenge submission for question: 3314 Construct the Minimum Bitwise Array I

---
+++ 3314 Construct the Minimum Bitwise Array I/3314construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int x:nums){
+            int val=-1;
+            for(int i=1;i<x;i++){
+                if((i | (i+1)) == x){
+                    val=i;
+                    break;
+                }
+            }
+            ans.push_back(val);
+        }
+        return ans;
+    }
+};

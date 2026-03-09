f07bb0a1e1e253544750be4cdaa0b3c5b5840c63
kunjpatel6151 <kunjkumar6151@gmail.com>
2026-03-09 16:45:16 +0000

    Code challenge submission for question: 1312 Minimum Insertion Steps to Make a String Palindrome

---
+++ 1312 Minimum Insertion Steps to Make a String Palindrome/1312minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return s.size() - lcs(s,t);
+    }
+
+    int lcs(string &s1, string &s2) {
+        // code here
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        if(s1[0]==s2[0]){
+            dp[0][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0];
+            if(s1[i]==s2[0]){
+                dp[i][0]=1;
+            }
+        }
+        for(int j=1;j<n;j++){
+            dp[0][j]=dp[0][j-1];
+            if(s1[0]==s2[j]){
+                dp[0][j]=1;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(s1[i]==s2[j]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i][j-1]});
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};

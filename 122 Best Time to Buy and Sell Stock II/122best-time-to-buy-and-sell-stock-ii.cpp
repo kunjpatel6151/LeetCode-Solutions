93ec7cb78ac3be1edeff52c901bf0aa465ab95b7
kunjpatel6151 <kunjkumar6151@gmail.com>
2026-03-12 13:51:45 +0000

    Code challenge submission for question: 122 Best Time to Buy and Sell Stock II

---
+++ 122 Best Time to Buy and Sell Stock II/122best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n, vector<int> (2,-1));
+        return solve(0,1,prices,dp);
+        
+    }
+
+    int solve(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
+        if(i>=prices.size()){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        if(buy){
+            dp[i][buy]=max(-prices[i]+solve(i+1,0,prices,dp),solve(i+1,1,prices,dp));
+        }
+        else{
+            dp[i][buy]=max(prices[i]+solve(i+1,1,prices,dp),solve(i+1,0,prices,dp));
+        }
+        return dp[i][buy];
+    }
+};

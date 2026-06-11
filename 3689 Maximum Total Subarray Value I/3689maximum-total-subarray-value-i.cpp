8acbff387d1aef7d74ee9f23e9b909e62e3e7150
kunjpatel6151 <kunjkumar6151@gmail.com>
2026-06-11 17:17:29 +0000

    Code challenge submission for question: 3689 Maximum Total Subarray Value I

---
+++ 3689 Maximum Total Subarray Value I/3689maximum-total-subarray-value-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        long long min_val=nums[0];
+        long long max_val=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<min_val){
+                min_val=nums[i];
+            }
+            if(nums[i]>max_val){
+                max_val=nums[i];
+            }
+        }
+        return k*(max_val-min_val);
+    }
+};
